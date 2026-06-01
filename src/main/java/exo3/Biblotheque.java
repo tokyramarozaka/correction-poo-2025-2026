5ae7657fd737613438f5288b3478b6e982da6154
fatratra-png <nyfatratrar@gmail.com>
2026-06-01 15:22:33 +0300

    feat:findByTitle method added

--- src/main/java/exo3/Biblotheque.java
+++ src/main/java/exo3/Biblotheque.java
@@ -6,87 +6,91 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Biblotheque {
-    private int id;
-    private String nom;
-    private String adresse;
-    private String ville;
-    private List<Livre> livres;
+  private int id;
+  private String nom;
+  private String adresse;
+  private String ville;
+  private List<Livre> livres;
 
-    public Biblotheque(int id, String nom, String adresse, String ville) {
-        this.id = id;
-        this.nom = nom;
-        this.adresse = adresse;
-        this.ville = ville;
-        this.livres = new ArrayList<>();
+  public Biblotheque(int id, String nom, String adresse, String ville) {
+    this.id = id;
+    this.nom = nom;
+    this.adresse = adresse;
+    this.ville = ville;
+    this.livres = new ArrayList<>();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public String getAdresse() {
+    return adresse;
+  }
+
+  public void setAdresse(String adresse) {
+    this.adresse = adresse;
+  }
+
+  public String getVille() {
+    return ville;
+  }
+
+  public void setVille(String ville) {
+    this.ville = ville;
+  }
+
+  public List<Livre> getLivres() {
+    return livres;
+  }
+
+  public void setLivres(List<Livre> livres) {
+    this.livres = livres;
+  }
+
+  public void ajouterLivre(Livre livre) {
+    if (livre == null) {
+      throw new IllegalArgumentException("Le livre ne peut pas être null");
+    }
+    this.livres.add(livre);
+  }
+
+  public void retirerLivreParId(int id) {
+    var existeDansLaBibliotheque = this.livres.stream()
+        .anyMatch(livre -> livre.getId() == id);
+    if (!existeDansLaBibliotheque) {
+      throw new IllegalArgumentException("L'id que vous avez donné n'existe pas dans la bibliotheque " + this.nom);
+    }
+    this.livres.removeIf(livre -> livre.getId() == id);
+  }
+
+  public List<Livre> livresTrieParTitreCroissant() {
+    return this.livres.stream()
+        .sorted(Comparator.comparing(Livre::getTitre))
+        .collect(Collectors.toList());
+  }
+
+  public List<Livre> chercherParTitre(String titre){
+      return this.livres.stream().filter(Livre livre->livre.getTitre().toLowerCase().contains(titre.toLowerCase()).toList();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
-    }
-
-    public String getVille() {
-        return ville;
-    }
-
-    public void setVille(String ville) {
-        this.ville = ville;
-    }
-
-    public List<Livre> getLivres() {
-        return livres;
-    }
-
-    public void setLivres(List<Livre> livres) {
-        this.livres = livres;
-    }
-
-    public void ajouterLivre(Livre livre) {
-        if (livre == null) {
-            throw new IllegalArgumentException("Le livre ne peut pas être null");
-        }
-        this.livres.add(livre);
-    }
-
-    public void retirerLivreParId(int id) {
-        var existeDansLaBibliotheque = this.livres.stream()
-                .anyMatch(livre -> livre.getId() == id);
-        if (!existeDansLaBibliotheque) {
-            throw new IllegalArgumentException("L'id que vous avez donné n'existe pas dans la bibliotheque " + this.nom);
-        }
-        this.livres.removeIf(livre -> livre.getId() == id);
-    }
-
-    public List<Livre> livresTrieParTitreCroissant(){
-        return this.livres.stream()
-                .sorted(Comparator.comparing(Livre::getTitre))
-                .collect(Collectors.toList());
-    }
-
-    public List<Livre> chercherParMotCle(String motCle) {
-        return this.livres
-                .stream()
-                .filter(livre -> livre.getResume().toLowerCase()
-                        .contains(motCle.toLowerCase()))
-                .collect(Collectors.toList());
-    }
+  public List<Livre> chercherParMotCle(String motCle) {
+    return this.livres
+        .stream()
+        .filter(livre -> livre.getResume().toLowerCase()
+            .contains(motCle.toLowerCase()))
+        .collect(Collectors.toList());
+  }
 }
